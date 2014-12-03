266e9ac6d18f60eff0dda817769fe71a4a79893e
willis7 <sion5@hotmail.co.uk>
2014-12-03 23:25:08 +0000

    Listing 3.12 Accessing a User's posts by walking the object graph

--- test/integration/com/grailsinaction/PostIntegrationSpec.groovy
+++ test/integration/com/grailsinaction/PostIntegrationSpec.groovy
@@ -10,7 +10,7 @@ class PostIntegrationSpec extends IntegrationSpec {
     def cleanup() {
     }
 
-    void "Adding posts to user links post to user"() {
+    def "Adding posts to user links post to user"() {
         given: "A brand new user"
         def user = new User(loginId: 'joe', password: 'secret')
         user.save(failOnError: true)
@@ -22,5 +22,23 @@ class PostIntegrationSpec extends IntegrationSpec {
 
         then: "The user has a list of posts attached"
         3 == User.get(user.id).posts.size()
+    }
+
+    def "Ensure posts linked to a user can be retrieved"() {
+        given: "A user with several posts"
+        def user = new User(loginId: 'joe', password: 'secret')
+        user.addToPosts(new Post(content: 'First'))
+        user.addToPosts(new Post(content: 'Second'))
+        user.addToPosts(new Post(content: 'Third'))
+        user.save(failOnError: true)
+
+        when: "The user is retrieved by their id"
+        def foundUser = User.get(user.id)
+        def sortedPostContent = foundUser.posts.collect {
+            it.content
+        }.sort()
+
+        then: "The posts appear on the retrieved user"
+        sortedPostContent == ['First', 'Second', 'Third']
     }
 }
