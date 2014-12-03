160e1010f2ba227ffc404dedae5030e98defc5e2
willis7 <sion5@hotmail.co.uk>
2014-12-03 22:46:52 +0000

    Listing 3.9 Adding a 1:1 relationship from user to profile

--- grails-app/domain/com/grailsinaction/User.groovy
+++ grails-app/domain/com/grailsinaction/User.groovy
@@ -5,8 +5,11 @@ class User {
     String password
     Date dateCreated
 
+    static hasOne = [ profile: Profile ]
+
     static constraints = {
         loginId size: 3..20, unique: true, nullable: false
-        password size: 6..8, nullable: false
+        password size: 6..8, blank: false, validator: { passwd, user -> passwd != user.loginId }
+        profile nullable: true
     }
 }
