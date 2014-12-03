b64adc825781b18998b0426dfb0f33fd5e276fa8
willis7 <sion5@hotmail.co.uk>
2014-12-03 22:40:54 +0000

    Listing 3.8 Refactored profile class with 1:1 relationship with the user class

--- grails-app/domain/com/grailsinaction/User.groovy
+++ grails-app/domain/com/grailsinaction/User.groovy
@@ -3,12 +3,10 @@ package com.grailsinaction
 class User {
     String loginId
     String password
-    String homepage
     Date dateCreated
 
     static constraints = {
         loginId size: 3..20, unique: true, nullable: false
         password size: 6..8, nullable: false
-        homepage url: true, nullable: true
     }
 }
