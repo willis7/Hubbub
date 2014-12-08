7acca4db7a378c976d496cd6bf7c26b9ff251751
willis7 <sion5@hotmail.co.uk>
2014-12-08 22:21:37 +0000

    Listing 5.5 Added lots of examples of writing where queries.

---
+++ test/integration/com/grailsinaction/QueryIntegrationSpec.groovy
@@ -0,0 +1,62 @@
+package com.grailsinaction
+
+import grails.test.spock.IntegrationSpec
+
+/**
+ * All of these queries require the database to be pre-populated. This is being managed by bootStrap.groovy
+ */
+class QueryIntegrationSpec extends IntegrationSpec {
+
+    def "Simple property comparison"() {
+        when: "Users are selected by a simple password match"
+        def users = User.where {
+            password == "testing"
+        }.list(sort: "loginId")
+
+        then: "The users with the same password are returned"
+        users*.loginId == ['frankie']
+    }
+
+    def "Multiple criteria"() {
+        when: "A user is selected by loginId or password"
+        def users = User.where {
+            loginId == 'frankie' || password == 'crikey'
+        }.list(sort: 'loginId')
+
+        then: "The matching loginIds are returned"
+        users*.loginId == ['dillon', 'frankie', 'sara']
+    }
+
+    def "Query on association"() {
+        when: "The 'following' collection is queried"
+        def users = User.where {
+            following.loginId == 'sara'
+        }.list(sort: 'loginId')
+
+        then: "A list of followers of the given user is returned"
+        users*.loginId == ['phil']
+    }
+
+    def "Query against a range value"() {
+        given: "The current date and time"
+        def now = new Date()
+
+        when: "The 'dateCreated' property is queried"
+        def users = User.where {
+            dateCreated in (now.minus(1))..now
+        }.list(sort: 'loginId', order: 'desc')
+
+        then: "The users created in the range are returned"
+        users*.loginId == ['phil', 'peter', 'glen', 'frankie', 'chuck_norris', 'admin']
+    }
+
+    def "Retrieve a single instance"() {
+        when: "A specific user is queried with get()"
+        def user = User.where {
+            loginId == 'phil'
+        }.get()
+
+        then: "A single instance is returned"
+        user.password == "thomas"
+    }
+}
